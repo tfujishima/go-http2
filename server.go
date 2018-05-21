174d239271b2d0e125411d70778fd0182e413d0c
Tomoaki FUJISHIMA <tomoaki-fujishima@gmo.jp>
2018-05-21 14:11:16 +0900

    静的ファイルの表示

--- server.go
+++ server.go
@@ -3,15 +3,19 @@ package main
 import(
  "net/http"
  "fmt"
+ "strings"
  "log"
 )
-func handle(w http.ResponseWriter, r *http.Request){
-  fmt.Fprint(w, "Hello world!!")
-}
 func main(){
-  http.HandleFunc("/",handle)
+  fileServer := http.StripPrefix("/static/",http.FileServer(http.Dir("static")))
   fmt.Println("server start port 443")
-  err := http.ListenAndServeTLS(":443","/etc/letsencrypt/live/kurone.iceclover.net/fullchain.pem","/etc/letsencrypt/live/kurone.iceclover.net/privkey.pem",nil)
+  err := http.ListenAndServeTLS(":443","/etc/letsencrypt/live/kurone.iceclover.net/fullchain.pem","/etc/letsencrypt/live/kurone.iceclover.net/privkey.pem", http.HandlerFunc( func(w http.ResponseWriter, r *http.Request){
+    if strings.HasPrefix(r.URL.Path, "/static/"){
+      fileServer.ServeHTTP(w,r)
+    }else{
+      fmt.Fprint(w, "Hello world!!")
+    }
+  }))
   if err != nil {
     log.Fatal("ListenAndServe: ", err)
   }
