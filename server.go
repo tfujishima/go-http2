ffc091f28742793d71d36b81edc0f727a6354394
Tomoaki FUJISHIMA <tomoaki-fujishima@gmo.jp>
2018-06-19 00:22:21 +0900

    snakeをcamelに

--- server.go
+++ server.go
@@ -15,9 +15,9 @@ func main() {
 	for _, v := range conf.Server {
 		wg.Add(1)
 		mux := http.NewServeMux()
-		mux.HandleFunc("/", create_server_handle(v.WebRoot, v.Index, conf.Php))
+		mux.HandleFunc("/", createServerHandle(v.WebRoot, v.Index, conf.Php))
 		for _, vhost := range v.Vhosts {
-			mux.HandleFunc(vhost.Name+"/", create_server_handle(vhost.WebRoot, vhost.Index, conf.Php))
+			mux.HandleFunc(vhost.Name+"/", createServerHandle(vhost.WebRoot, vhost.Index, conf.Php))
 		}
 		log.Println("server start port ", v.Port)
 		go func(server config.Server) {
@@ -36,18 +36,18 @@ func main() {
 	wg.Wait()
 }
 
-func create_server_handle(web_root string, index string, php config.Php) func(http.ResponseWriter, *http.Request) {
-	fileServer := http.StripPrefix("/", http.FileServer(http.Dir(web_root)))
+func createServerHandle(webRoot string, index string, php config.Php) func(http.ResponseWriter, *http.Request) {
+	fileServer := http.StripPrefix("/", http.FileServer(http.Dir(webRoot)))
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method + ": " + web_root + r.URL.Path)
+		log.Println(r.Method + ": " + webRoot + r.URL.Path)
 		if index != "index.html" && r.URL.Path[len(r.URL.Path)-1:] == "/" {
 			r.URL.Path += index
 		}
 		if php.Enabled && strings.HasSuffix(r.URL.Path, ".php") {
 			if r.Method == "GET" {
-				phpfpm.Get(w, r, web_root, php.FpmSock)
+				phpfpm.Get(w, r, webRoot, php.FpmSock)
 			} else if r.Method == "POST" {
-				phpfpm.Post(w, r, web_root, php.FpmSock)
+				phpfpm.Post(w, r, webRoot, php.FpmSock)
 			} else {
 				phpfpm.OtherHttpMethod(w)
 			}
