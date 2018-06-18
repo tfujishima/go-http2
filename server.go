7a90276793c3b19b702be74008fb203a80610239
Tomoaki FUJISHIMA <tomoaki-fujishima@gmo.jp>
2018-06-19 00:20:13 +0900

    php-fpm周りを別パッケージに

--- server.go
+++ server.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"./config"
-	"bytes"
-	"github.com/tomasen/fcgi_client"
-	"io/ioutil"
+	"./phpfpm"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 	"sync"
 )
@@ -47,39 +44,13 @@ func create_server_handle(web_root string, index string, php config.Php) func(ht
 			r.URL.Path += index
 		}
 		if php.Enabled && strings.HasSuffix(r.URL.Path, ".php") {
-			fcgi, err := fcgiclient.Dial("unix", php.FpmSock)
-			if err != nil {
-				log.Fatal("err@sock:", err)
-			}
-			env := make(map[string]string)
-			var resp *http.Response
-			env["SCRIPT_FILENAME"] = web_root + "/" + r.URL.Path
 			if r.Method == "GET" {
-				env["SERVER_SOFTWARE"] = "go / fcgiclient "
-				env["REMOTE_ADDR"] = "127.0.0.1"
-				env["QUERY_STRING"] = r.URL.RawQuery
-				resp, err = fcgi.Get(env)
+				phpfpm.Get(w, r, web_root, php.FpmSock)
 			} else if r.Method == "POST" {
-				r.ParseForm()
-				resp, err = fcgi.PostForm(env, r.Form)
+				phpfpm.Post(w, r, web_root, php.FpmSock)
 			} else {
-				resp = &http.Response{
-					Body:       ioutil.NopCloser(bytes.NewBufferString("This http method is not supported.")),
-				}
-				w.WriteHeader(501)
+				phpfpm.OtherHttpMethod(w)
 			}
-			if err != nil {
-				log.Fatal("err@resp:", err)
-			}
-			content, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal("err@body:", err)
-			}
-			if resp.Header.Get("Status") != "" {
-				status_code, _ := strconv.Atoi(resp.Header.Get("Status")[:3])
-				w.WriteHeader(status_code)
-			}
-			w.Write([]byte(content))
 		} else {
 			fileServer.ServeHTTP(w, r)
 		}
